5939b755a19cc5dd4c6722728b52d2fab0709f81
damnhandin <lushnikovff@gmail.com>
2024-03-06 17:51:10 +0300

    create blog with posts interaction

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,14 +9,24 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    # представления поста
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
+]urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
