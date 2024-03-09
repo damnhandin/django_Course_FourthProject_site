ccd6587eb052e241b3ac6710a0fabf201970024e
damnhandin <lushnikovff@gmail.com>
2024-03-09 17:20:40 +0300

    add little seo opt

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,8 @@ else:
 
 # Application definition
 
+SITE_ID = 1
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,7 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'taggit'
+    'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps'
 ]
 
 MIDDLEWARE = [
